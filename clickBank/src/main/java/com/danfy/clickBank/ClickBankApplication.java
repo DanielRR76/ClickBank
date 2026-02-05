689b04ad3f16d0997b3afc6ee48eed1120aa95a3
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-02-05 15:17:56 -0300

    Add Maven Wrapper and initial project structure with Spring Boot setup

---
+++ clickBank/src/main/java/com/danfy/clickBank/ClickBankApplication.java
@@ -0,0 +1,13 @@
+package com.danfy.clickBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickBankApplication.class, args);
+	}
+
+}
---
+++ clickBank/src/test/java/com/danfy/clickBank/ClickBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danfy.clickBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
